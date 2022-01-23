74f57708ee4b969f8c8c3aba998954721cbdcf4b
Jonah <jonahgoh@outlook.com>
2022-01-23 17:38:44 +0800

    Add Sign in and Sign up pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import Navibar from "./Navibar"
+import logo from "./logo.svg";
+import "./App.css";
+import Navibar from "./Navibar";
 
 function App() {
   return (
-      <div className="App">
-        <Navibar />
-      </div>
+    <div className="App">
+      <Navibar />
+    </div>
   );
 }
 
 export default App;
-
